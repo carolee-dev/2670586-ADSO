0e6c921663426af83ed6e82faf31aa0c2179dfd2
INSTRUCTOR <INSTRUCTOR@PERAPRCSAFSD854.FormacionSena.RED>
2023-05-13 00:45:34 +0200

    Trabajos y ejercicios del viernes 12 de mayo

---
+++ 2670586_ADSO-Carolee/02_Ejercicios/Ejercicio01.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Ejercicio01{
+    public static void main(String[] args){
+        // crear variable de entrada
+        Scanner entrada = new Scanner(System.in);
+
+        int num_1;
+        int num_2;
+        int result;
+
+        System.out.println("Ingrese primer número: ");
+        num_1 = entrada.nextInt();
+
+    
+    git add .
+    git commit -m""
+    git push origin
