55dc81dcdd16e1824bf62a81b6f3b48f033bfe14
Carolina Lee <carolina.chumin@gmail.com>
2023-05-23 11:59:42 -0500

    Corrección Ejercicio014 variables calcular promedio ponderado

--- 02_EjerciciosVariables/Ejercicio14.java
+++ 02_EjerciciosVariables/Ejercicio14.java
@@ -4,8 +4,8 @@ public class Ejercicio14 {
     public static void main(String[]args){
         Scanner teclado = new Scanner(System.in);
 
-        float not1, not2, not3, not4, sumNot, pond;
-        int cred1, cred2, cred3, cred4, sumCred;
+        float not1, not2, not3, pond;
+        int cred1, cred2, cred3, sumCred;
 
         System.out.println("De 1 a 10, ingrese primera nota: ");
         not1 = teclado.nextFloat();
@@ -13,9 +13,6 @@ public class Ejercicio14 {
         not2 = teclado.nextFloat();
         System.out.println("De 1 a 10, ingrese tercera nota: ");
         not3 = teclado.nextFloat();
-        System.out.println("De 1 a 10, ingrese cuarta nota: ");
-        not4 = teclado.nextFloat();
-        // sumNot = not1 + not2 + not3 + not4;
 
         System.out.println("De 1 a 5, ingrese créditos de la materia 1: ");
         cred1 = teclado.nextInt();
@@ -23,13 +20,10 @@ public class Ejercicio14 {
         cred2 = teclado.nextInt();
         System.out.println("De 1 a 5, ingrese créditos de la materia 3: ");
         cred3 = teclado.nextInt();
-        System.out.println("De 1 a 5, ingrese créditos de la materia 4: ");
-        cred4 = teclado.nextInt();
-        sumCred = cred1 + cred2 + cred3 + cred4;
+    
+        sumCred = cred1 + cred2 + cred3;
 
-        pond = (not1*cred1)+(not2*cred2)+(not3*cred3)+(not4*cred4)/sumCred;
-
-        // pond = (sumNot*sumCred)/sumCred;
+        pond = ((not1*cred1)+(not2*cred2)+(not3*cred3))/sumCred;
 
         System.out.println("El promedio ponderado del estudiante es: "+pond);
 
