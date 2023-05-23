e4ae83d3a9bbc5e53831bad576fd0503367ab4e4
Carolina Lee <carolina.chumin@gmail.com>
2023-05-23 16:02:37 -0500

    Corrección Ex02 Condiciones con pipe :)

--- 03_EjerciciosCondiciones/Ex02_parImpar.java
+++ 03_EjerciciosCondiciones/Ex02_parImpar.java
@@ -4,24 +4,21 @@ public class Ex02_parImpar {
     public static void main(String[]args){
         Scanner entrada = new Scanner(System.in);
 
+        int numero;
+
         System.out.println("Hola usuario, ingresa un número de 3 dígitos y te diré si es par o impar: ");
-        int numero = entrada.nextInt();
+        numero = entrada.nextInt();
 
-        if (numero <= 100){
+        if (numero <= 100 || numero >= 999){
             System.out.println("El número deber ser de 3 digitos. ");
-            }
-        else{ 
-            if (numero%2 == 0){
-                System.out.println("El número es par. ");
-            }
-            else{
-                System.out.println("El número es impar. ");
-            }
         }
-
-        if (numero >= 999) {
-            System.out.println("...Pero no es válido. ");
-        }
+        else
+            {if (numero % 2 == 0)
+                {System.out.println("El número es par. ");}
+            else
+                {System.out.println("El número es impar. ");}
+            }
+    
     }    
     
 }
