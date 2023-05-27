4046d44c234f5dd1c0f7be23b98694f2f96b2a2c
Carolina Lee <carolina.chumin@gmail.com>
2023-05-26 22:48:29 -0500

    Actualización después de clase.

--- 03_EjerciciosCondiciones/Ex07_separarMiles.java
+++ 03_EjerciciosCondiciones/Ex07_separarMiles.java
@@ -10,16 +10,73 @@ public class Ex07_separarMiles {
 
         System.out.println("// ---------- FORMATO DE MONEDA ----------- //");
 
+        /*
         int miles1 = (nums % 1000000000) / 1000000;
         int miles2 = (nums % 1000000) / 1000;
         int miles3 = (nums % 1000) / 1;
         int miles4 = (nums % 10);
+        */
 
-        if (nums > 2147483647){
-            System.out.println("Ingrese número no mayor a 2.147.483.647 ");
+        if (nums >= 0 && nums <= 2147483647){
+            
+            int cientos = nums % 1000;
+            int miles = (nums / 1000) % 1000;
+            int millones = (nums / 1000000) % 1000;
+            int miles_millones = numero / 1000000000;
+
+            System.out.println("Moneda: "+miles_millones+"."+millones+"."+miles+"."+cientos);
+
+            // Algoritmo print miles_millones
+            if(miles_millones != 0){
+                System.out.println(+miles_millones+".");
+            }else{
+                System.out.println("");
+            }
+
+            // Algoritmo print millones
+            if(millones == 0){
+                if(miles_millones !=0){
+                    System.out.print("000.");
+                }else{
+                    System.out.print("");
+                }
+            }else{
+                // Sea X
+                if(millones >=1 && millones <= 9){
+                    if(miles_millones != 0){
+                        System.out.print("00"+millones+".");
+                    }else{
+                        System.out.print("");
+                    }
+                    
+                }
+                if(millones >=10 && millones <= 99){
+
+                }
+                if(millones >=100 && millones <= 999){
+
+                }
+
+            }
+
+            // Algoritmo print miles
+
+
+
+
+            
+        }else{
+            System.out.println("Ingrese un número válido. ");
         }
 
-        if (nums >= 1000000000 && nums <= 2147483647){
+
+
+
+
+
+
+
+        /* if (nums >= 1000000000 && nums <= 2147483647){
             System.out.println("Moneda: $ "+miles1+"."+miles2+"."+miles3+"."+miles4);
         }
 
