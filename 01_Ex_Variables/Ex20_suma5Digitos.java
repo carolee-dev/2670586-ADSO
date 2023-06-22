57beaa6d9edcfd693a337cc6cf1a10dcd56f2cd6
Carolina Lee <carolina.chumin@gmail.com>
2023-06-22 00:09:37 -0500

    Compilado Ex20_suma5Digitos.

--- 01_Ex_Variables/Ex20_suma5Digitos.java
+++ 01_Ex_Variables/Ex20_suma5Digitos.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
 
-public class Ex20_sumaDigits{
+public class Ex20_suma5Digitos{
     public static void main(String[]args){
         Scanner entrada = new Scanner(System.in);
 
-        System.out.println("Ingrese número (máximo 5 dígitos): ");
+        System.out.print(" Ingresa un número máximo de 5 dígitos: ");
         int numero = entrada.nextInt();
 
         /* 
@@ -21,14 +21,14 @@ public class Ex20_sumaDigits{
         int digito4 = (numero%100)/10;
         int digito5 = (numero%10);
 
-        System.out.println(" Digito 1 es: "+digito1);
-        System.out.println(" Digito 2 es: "+digito2);
-        System.out.println(" Digito 3 es: "+digito3);
-        System.out.println(" Digito 4 es: "+digito4);
-        System.out.println(" Digito 5 es: "+digito5);
+        System.out.println(" Dígito 1 es: "+digito1);
+        System.out.println(" Dígito 2 es: "+digito2);
+        System.out.println(" Dígito 3 es: "+digito3);
+        System.out.println(" Dígito 4 es: "+digito4);
+        System.out.println(" Dígito 5 es: "+digito5);
 
         int resultado = digito1 + digito2 + digito3 + digito4 + digito5;
-        System.out.println("La suma de los dígitos es: "+resultado);
+        System.out.println(" La suma de los dígitos es: "+resultado);
 
     }
 }
