c4b5d9caa1d21a8229ad2c725f1c2530252c99f8
Carolina Lee <carolina.chumin@gmail.com>
2023-06-23 14:56:15 -0500

    Ex01 de Act05 Condicional.

--- 05_Ex_Ciclos/Ex01_numRandom.java
+++ 05_Ex_Ciclos/Ex01_numRandom.java
@@ -5,20 +5,24 @@ public class Ex01_numRandom{
         Scanner lectura = new Scanner(System.in);
 
         int num_user = 1;
+        int min = 0; 
+        int max = 9; 
+        int num_random = 0;
 
         System.out.print("-->Ingrese un número entero entre 1 y 6: ");
         num_user = lectura.nextInt();
 
-        for(int i=1; num_user >= 1 && num_user <=6; i=i+1){
-            int min = num_user, max = 9;
-            int num_random = 0; 
-            num_random = (int)Math.floor(Math.random()*(max-min+1)+min);
-            System.out.print("El número construído es: "+num_random);
+        if(num_user >= 1 && num_user <= 6){
+            num_random = (int)(Math.random() * (max - min + 1) + min);
+            System.out.println("-->El número construído es: "+num_random);
+            
+            int producto = num_random * 2;
+            System.out.println("-->Resultado producto: "+producto);
+
+        }else{
+            System.out.println("-->Valor ingresado no válido. ");
         }
-
-        int producto = num_random * 2;
-
-        System.out.println("Resultado producto: "+producto);
+        
 
     }
 }
