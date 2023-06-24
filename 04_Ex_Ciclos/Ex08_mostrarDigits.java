885f29aec2555328a0f2556849db0e4f41c813c7
Carolina Lee <carolina.chumin@gmail.com>
2023-06-23 23:01:05 -0500

    Ex08 de Act04 de ciclos: avance en contador de dígitos y extracción last digit.

--- 04_Ex_Ciclos/Ex08_mostrarDigits.java
+++ 04_Ex_Ciclos/Ex08_mostrarDigits.java
@@ -8,13 +8,13 @@ public class Ex08_mostrarDigits{
         int numero = lector.nextInt();
 
         if(numero >= 0 && numero <= 999999){
+            int digito = numero % 10;
 
             for(int i = 1; i <= numero || numero != 0; i++){
                 numero = numero / 10;
-                System.out.println("--> El dígito "+i+" es: ");
+                System.out.println("--> El dígito "+i+" es: "+digito);
             }
-            
-    
+                
 
         }else{
             System.out.println("--> Ingrese un número válido.");
