20c3e40d2608f4d92020f23c54663447239cafe9
Carolina Lee <carolina.chumin@gmail.com>
2023-07-09 20:38:33 -0500

    Act04, Ex07 Mínimo Común Múltiplo.

---
+++ 04_Ex_Ciclos/Ex07_MinCoMultiplo.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class Ex07_MinCoMultiplo{
+    public static void main(String[] args){
+        Scanner lector = new Scanner(System.in);
+
+        int num1, num2, num3;
+
+        System.out.print("--> Ingrese el primer número: ");
+        num1 = lector.nextInt();
+
+        System.out.print("--> Ingrese el segundo número: ");
+        num2 = lector.nextInt();
+
+        System.out.print("--> Ingrese el tercer número: ");
+        num3 = lector.nextInt();
+
+        int numMayor = num1;
+        if(num2 > numMayor){
+            numMayor = num2;
+        }
+        if(num3 > numMayor){
+            numMayor = num3;
+        }
+
+        int mcm = numMayor;
+        while(numMayor != 0){
+            if(mcm % num1 == 0 && mcm % num2 == 0 && mcm % num3 == 0){
+                break;
+            }
+            mcm = mcm + numMayor;
+        }
+        System.out.println("--> El es MCM es: "+mcm);
+    }
+}
