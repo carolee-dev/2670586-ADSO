520c3ed9d969a95164124515e035cf68b35bc742
Carolina Lee <carolina.chumin@gmail.com>
2023-07-20 17:55:21 -0500

    Completado Act-04 de Ciclos. Cuadrícula mostrando primos en consecutivo :)

--- 04_Ex_Ciclos/Ex10_mostrarPrimos.java
+++ 04_Ex_Ciclos/Ex10_mostrarPrimos.java
@@ -10,33 +10,47 @@ public class Ex10_mostrarPrimos{
         
         if(N >= 0 && N <= 11){
 
-            int num, i, esPrimo, contador;
+            int num, i, esPrimo, contador, fila, columna;
 
-            for(int fila = 1; fila <= N; fila++){
+            for(fila = 1; fila <= N; fila++){
                 System.out.println();
-                for(num = 1; num <= N; num++){
 
-                    num = 2;
-                    contador = 0;
-                    while(contador < N){
-                        esPrimo = 1;
-                        i = 2;
+                num = 2;
+                contador = 0;
+                fila = 1;
+                columna = 1;
+                while(contador < N * N){
+                    esPrimo = 1;
+                    i = 2;
 
-                        while(i <= num / 2){
-                            if(num % i == 0){
-                                esPrimo = 0;
-                                i = num;
-                            }
-                            i = i + 1;
+                    while(i <= num / 2){
+                        if(num % i == 0){
+                            esPrimo = 0;
+                            i = num;
                         }
-                        if(esPrimo == 1){
-                            System.out.print("["+num+"]");
-                            contador = contador + 1;
-                        }
-                        num = num + 1;
+                        i = i + 1;
                     }
-                    
-                }
+                    if(esPrimo == 1){
+                        if(num <= 10){
+                            System.out.print("[00"+num+"]");
+                        }else if(num < 100){
+                            System.out.print("[0"+num+"]");
+                        }else{
+                            System.out.print("["+num+"]");
+                        }
+                        contador = contador + 1;
+
+                        if(contador % N == 0){
+                            fila = fila + 1;
+                            columna = 1;
+                            System.out.println("");
+                        }else{
+                            columna = columna + 1;
+                        }
+                    }
+                    num = num + 1;
+                }    
+                
             }
             System.out.println();
             System.out.println("|---------------------------------------------|");
