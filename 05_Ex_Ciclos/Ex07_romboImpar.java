936ada8b98effcaf63fef6664ed78707004958d1
Carolina Lee <carolina.chumin@gmail.com>
2023-07-28 22:03:03 -0500

    Adelanto Act05-Ex07 rombo impar.

--- 05_Ex_Ciclos/Ex07_romboImpar.java
+++ 05_Ex_Ciclos/Ex07_romboImpar.java
@@ -10,12 +10,30 @@ public class Ex07_romboImpar{
         
         // Hacer bandera de impar.
         if(N % 2 == 1){
-            
+            int alto = 0;
+            int ancho = 0;
+            int espacio = 0;
+            int equis = 0;
 
+            
+            for(alto = 1; alto <= N; alto++){
+                //System.out.print(alto);
+                for(ancho = 1; ancho <= N; ancho++){
+                    //System.out.print(ancho);
+                    for(espacio = 1; espacio <= ancho-1; espacio++){
+                        System.out.print("-");
+                    }
+                    for(equis = 1; equis <= ancho; equis++){
+                        System.out.print("x");
+                    }
+                }
+                
+            System.out.println(" ");
+            }
             
 
         }else{
-            System.out.println("--> Número no es impar. ");
+            System.out.println("--> Número debe ser impar. ");
         }
 
     }
