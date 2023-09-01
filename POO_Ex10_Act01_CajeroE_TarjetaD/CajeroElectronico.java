382e681ed533efbc781fa9adaa140e658ec1404f
Carolina Lee <carolina.chumin@gmail.com>
2023-09-01 17:49:59 -0500

    Agregando avance en clase.

--- POO_Ex10_Act01_CajeroE_TarjetaD/CajeroElectronico.java
+++ POO_Ex10_Act01_CajeroE_TarjetaD/CajeroElectronico.java
@@ -1,38 +1,57 @@
+import java.util.Date;
+
 public class CajeroElectronico {
 
     // Atributos:
-    private int cantidadDineroTotal;
-    private int cantidadDineroDisponible;
-    int billete_10;
+    int capacidadTotal; // que le cabe al cajero
+    int dineroDisponible;
+    int billete_10; // cantidad de billetes, no monto
     int billete_20;
     int billete_50;
     int billete_100;
     String nombreBanco;
     String listaTransacciones[]; // (fecha-tipoTransaccion-monto-estado)
-    private String usuarioAdmin;
-    private String claveAdmin;
+    String usuarioAdmin;
+    String claveAdmin;
 
-    // Método Constructor para inicializar las variables:
-    public CajeroElectronico (int dato1, int dato2, int dato3, int dato4, int dato5, int dato6, String dato7, String dato8, String dato9, String dato10,) {
-        cantidadDineroTotal = dato1;
-        cantidadDineroDisponible = dato2;
-        billete_10 = dato3;
-        billete_20 = dato4;
-        billete_50 = dato5;
-        billete_100 = dato6;
-        nombreBanco = dato7;
-        listaTransacciones[] = dato8;
-        usuarioAdmin = dato9;
-        claveAdmin = dato10;
+    // Método CONSTRUCTOR para inicializar las variables: ESTE RECIBE 9 DATOS
+    public CajeroElectronico (int capacidadTotal, int dineroDisponible, int billete_10, int billete_20, int billete_50, int billete_100, String nombreBanco, String usuarioAdmin, String claveAdmin) {
+        this.capacidadTotal = capacidadTotal;
+        this.dineroDisponible = dineroDisponible;
+        this.billete_10 = billete_10;
+        this.billete_20 = billete_20;
+        this.billete_50 = billete_50;
+        this.billete_100 = billete_100;
+        this.nombreBanco = nombreBanco;
+        this.usuarioAdmin = usuarioAdmin;
+        this.claveAdmin = claveAdmin;
+
+        listaTransacciones = new String [20];
+    }
+
+    // Otro método CONSTRUCTOR para inicializar las variables: ESTE RECIBE 4 DATOS
+    public CajeroElectronico (int capacidadTotal, String nombreBanco, String usuarioAdmin, String claveAdmin) {
+        this.capacidadTotal = capacidadTotal;
+        this.nombreBanco = nombreBanco;
+        this.usuarioAdmin = usuarioAdmin;
+        this.claveAdmin = claveAdmin;
+
+        this.dineroDisponible = 0;
+        this.billete_10 = 0;
+        this.billete_20 = 0;
+        this.billete_50 = 0;
+        this.billete_100 = 0;
+
+        listaTransacciones = new String [20];
 
     }
 
     // Método Getter para extraer los valores:
     public int getCantidadDineroTotal () {
-        return cantidadDineroTotal;
+        return capacidadTotal;
     }
-    public int getCantidadDineroDisponible () {
-        return cantidadDineroDisponible;
+    public int getDineroDisponible () {
+        return dineroDisponible;
     }
     public int getBillete_10 () {
         return billete_10;
@@ -49,9 +68,9 @@ public class CajeroElectronico {
     public String getNombreBanco () {
         return nombreBanco;
     }
-    public String getListaTransacciones([]) {
-        return listaTransacciones[];
-    };
+    public String [] getListaTransacciones() {
+        return listaTransacciones;
+    }
     public String getUsuarioAdmin () {
         return usuarioAdmin;
     }
@@ -62,10 +81,10 @@ public class CajeroElectronico {
     // Método Setter para asignar o cambiar los valores de los 
     // atributos y recibir el nuevo dato a modificar:
     public void setCantidadDineroTotal (int dato) {
-        cantidadDineroTotal = dato;
+        capacidadTotal = dato;
     }
-    public void setCantidadDineroDisponible (int dato) {
-        cantidadDineroDisponible = dato;
+    public void setDineroDisponible (int dato) {
+        dineroDisponible = dato;
     }
     public void setBillete_10 (int dato) {
         billete_10 = dato;
@@ -83,23 +102,94 @@ public class CajeroElectronico {
         nombreBanco = dato;
     }
     public void setListaTransacciones (String[] dato) {
-        listaTransacciones[] = dato;
+        listaTransacciones = dato;
     }
-    public void setUsuarioAdmin () {
+    public void setUsuarioAdmin (String dato) {
         usuarioAdmin = dato;
     }
-    public void setClaveAdmin () {
+    public void setClaveAdmin (String dato) {
         claveAdmin = dato;
     }
 
     // Otros métodos a crear para la aplicación:
-        // abastecerCajero(...)
-        // bloquearCajero(...)
+        // abastecerCajero(...) 
         // verHistorialCajero(...)
         // consignarDineroTarjeta(...)
         // retirarDineroTarjeta(...)
         // consultarSaldoTarjeta(...)
-        // cambiarClave(...)
+        // cambiarClaveTarjeta(...)
+        // bloquearCajero(...) --> con boolean
+
+    
+    // Para ver que esté imprimiendo bien.
+    public void imprimirDetalle () {
+        System.out.println("\n------------------------------ ");
+        System.out.println(" capacidadTotal = " +capacidadTotal);
+        System.out.println(" dineroDisponible = "+dineroDisponible);
+        System.out.println(" billete_10 = "+billete_10);
+        System.out.println(" billete_20 = "+billete_20) ;
+        System.out.println(" billete_50 = "+billete_50);
+        System.out.println(" billete_100 = "+billete_100);
+        System.out.println(" nombreBanco = "+nombreBanco);
+    
+        System.out.println(" usuarioAdmin = "+usuarioAdmin);
+        System.out.println(" claveAdmin = "+claveAdmin);
+
+        for (int i = 0; i < listaTransacciones.length; i++) {
+            System.out.println(" Historial Transacciones = "+listaTransacciones[i]);
+        }
+
+        System.out.println(" ------------------------------ ");
+        System.out.println("");
+    }
+    // abastecerCajero(...)
+    public boolean abastecerCajero(int billete_10, int billete_20, int billete_50, int billete_100) {
+        // Calcular total abastecido
+        int total = (billete_10 * 10000) + (billete_20 * 20000) + (billete_50 * 50000) + (billete_100 * 100000);
+
+        // Validar que no exceda el límite
+        if (total <= capacidadTotal) {
+            // Si. Almacenar los datos en los atributos y retornar True
+            dineroDisponible = total;
+            this.billete_10 = billete_10; // atributo billete_10 = parámetro billete_10
+            this.billete_20 = billete_20;
+            this.billete_50 = billete_50;
+            this.billete_100 = billete_100;
+
+            registrarTransaccion("ABASTECER", "0001 0002 0003 0004", total, "OK");
+            return true;
+
+        } else {
+            // No. Imprimir mensaje y retornar False
+            registrarTransaccion("ABASTECER", "0001 0002 0003 0004", total, "ERROR");
+            return false;
+        }
+    }
+
+    public void registrarTransaccion (String tipo, String numeroTarjeta, int monto, String estado) {
+        // Construir texto que va dentro de la listaTransacciones
+        Date fecha = new Date(); // Objeto Date en JDK. Se instancia.
+                   //metodo .toString para 'jalar' la fecha.
+        String texto = fecha.toString()" - "+tipo+ " - "+numeroTarjeta+" - "+monto+" - "+estado;
+
+        // Buscar un índice donde esté vacío y meter
+        int indice = -1;
+        for (int i = 0; i < listaTransacciones.length; i++) {
+            if (listaTransacciones[i] == null) {
+                indice = i;
+                break; // para que pare el for al encontrar el primer null y muestre los registros en orden
+            }
+        }
+        if (indice != -1) {
+            listaTransacciones[indice] = texto;
+        } else {
+            for (int i = 0; i < listaTransacciones.length -1; i++) {
+                listaTransacciones[i] = listaTransacciones[i + 1];
+            }
+            listaTransacciones[listaTransacciones.length -1] = texto;
+        }
+
+    }
 
 
 
--- POO_Ex10_Act01_CajeroE_TarjetaD/Principal.java
+++ POO_Ex10_Act01_CajeroE_TarjetaD/Principal.java
@@ -1,6 +1,39 @@
+import java.util.Scanner;
+
 public class Principal {
     public static void main(String[] args) {
+        // Scanner entrada_numero = new Scanner(System.in);
+        // Scanner entrada_texto = new Scanner(System.in);
+
+        CajeroElectronico cajero_10 = new CajeroElectronico (30000000, 10000000, 100, 50, 20, 70, "Oscar Bank", "adso123", "1010");
+        cajero_10.imprimirDetalle();
+
+        CajeroElectronico cajero_20 = new CajeroElectronico (150000000, "Oscar Bank", "Admin_3", "2010");
+        cajero_20.imprimirDetalle();
+
+        cajero_20.abastecerCajero(1000, 1000, 500, 500);
+        cajero_20.abastecerCajero(1000, 1000, 500, 5000);
+        cajero_20.imprimirDetalle();
+
+
+
+        CajeroElectronico listaCajeros [] = new CajeroElectronico [2];
+        int indiceCajeros = 0;
+        listaCajeros[0] = new CajeroElectronico(50000000, 50000000, 10000000, 10000000, 10000000, 20000000, "Oscar Bank", "admin Carlos", "5050");
+        listaCajeros[1] = new CajeroElectronico(50000000, 50000000, 10000000, 10000000, 10000000, 20000000, "Oscar Bank", "admin Carlos", "4040");
+        
+        // TarjetaDebito listaTarjetas [] = new TarjetaDebito [5];
+        // int indiceTarjetas = 0;
+        // listaTarjetas[0] = new TarjetaDebito("Primera Zuluaga", "0001 1234 1234", "Banco Gro", 900000, 890000, "ultimasTransacciones", "1010", "Estado");
+        // listaTarjetas[1] = new TarjetaDebito("Segundo Perez", "0002 1234 1234", "Banco Mundo", 800000, 790000, "ultimasTransacciones", "2020", "Estado");
+        // listaTarjetas[2] = new TarjetaDebito("Tercero López", "0003 1234 1234", "Banco Mundo", 700000, 690000, "ultimasTransacciones", "3030", "Estado");
+        // listaTarjetas[3] = new TarjetaDebito("Cuarto Jaramillo", "0004 1234 1234", "Banco Gro", 600000, 590000, "ultimasTransacciones", "4040", "Estado");
+        // listaTarjetas[4] = new TarjetaDebito("Quinto Tobón", "0005 1234 1234", "Banco Mundo", 500000, 490000, "ultimasTransacciones", "5050", "Estado");
 
         
+
+
+
+
     }
 }
