c90ec5a0cf4d09ecda63e1e12621587c9e428e54
Carolina Lee <carolina.chumin@gmail.com>
2023-09-15 18:41:36 -0500

    Actualizando 13_RepasoFunctions con Example02 y comentarios.

--- 13_RepasoFunctions/Example01.java
+++ 13_RepasoFunctions/Example01.java
@@ -2,18 +2,7 @@ public class Example01{
 
     public static void main(String[] whatever){
 
-        int listado[] = new int [10];
-        imprimirArreglo( listado );
-        listado = llenarArreglo( listado );
-        imprimirArreglo( listado );
 
-        String listadoPersonas [] = new String[5];
-        imprimirArregloTexto(listadoPersonas);
-        listadoPersonas = restablecerArreglo( listadoPersonas );
-        imprimirArregloTexto( listadoPersonas);
-
-
-        /*
         System.out.println("\n----- PROBANDO FUNCIONES -----");
         System.out.println(" Suma = "+sumar(5, 3));
         System.out.println(" Suma = "+sumar(3, 2));
@@ -27,7 +16,6 @@ public class Example01{
         System.out.println(" Dividir = "+dividir(-3, 20));
         System.out.println(" Dividir = "+dividir(100, -50));
 
-
         int yoyito = sumar(4, 4);
         System.out.println(" Yoyito = "+yoyito);
 
@@ -38,67 +26,46 @@ public class Example01{
         }
 
         saludar(); 
+
         saludarEnLoop(5);
 
         imprimirTabla(5, 10);
-        */
+        
+
+
     }
     
+    // función con int:
     public static int sumar(int a, int b){
         int resultado = (a + b);
         return resultado; 
     }
 
+    // función con float:
     public static float dividir(float c, float d){
         float resultado = (c / d);
         return resultado;   
     }
 
+    // función sin retorno, solo se ejecuta cuando se le invoca en el public static void main de arriba:
+    // void es para función que no tiene return
     public static void saludar(){
         System.out.println(" \n--- HOLA MUNDO --- \n");
     }
 
+    // función sin retorno, solo se ejecuta cuando se le invoca en el public static void main de arriba:
     public static void saludarEnLoop(int veces){
         for(int i = 0; i < veces; i++){
             System.out.println(" \n--- HOLA MUNDO "+i+"--- \n");    
         }
     }
 
+    // función sin retorno, solo se ejecuta cuando se le invoca en el public static void main de arriba:
     public static void imprimirTabla(int tabla, int multiplo){
         for(int i = 0; i <= multiplo; i++){
             int resultado = (tabla * i);
             System.out.println(" "+tabla+" x "+i+" = "+resultado);
         }
-    }
-
-    public static void imprimirArreglo(int [] arreglo ){ // indicar la dimensión antes del nombre del parámetro
-        for( int i = 0; i < arreglo.length; i++){
-            System.out.print(" [ "+arreglo[i]+" ] ");
-        }
-        System.out.println("");
-    }
-
-    public static int[] llenarArreglo(int [] arreglo){
-        for(int i = 0; i < arreglo.length; i++){
-            int aleatorio = (int) (Math.random()*100);
-            arreglo[i] = aleatorio; // En la posición del arreglo, recíbame los aleatorios
-        }
-        return arreglo;
-    }
-
-
-    public static String[] restablecerArreglo( String [] arreglo){
-        for (int i = 0; i < arreglo.length; i++){
-            arreglo[i] = "Llenito";
-        }
-        return arreglo;
-    }
-
-    public static void imprimirArregloTexto(String [] arreglo ){ // indicar la dimensión antes del nombre del parámetro
-        for( int i = 0; i < arreglo.length; i++){
-            System.out.print(" [ "+arreglo[i]+" ] ");
-        }
-        System.out.println("");
     }
 
 
---
+++ 13_RepasoFunctions/Example02.java
@@ -0,0 +1,55 @@
+public class Example02{
+
+    public static void main(String[] args){
+
+
+       int listado[] = new int [10];
+
+       imprimirArreglo( listado );
+       listado = llenarArreglo( listado );
+       imprimirArreglo( listado );
+       
+
+       String listadoPersonas [] = new String[5];
+
+       imprimirArregloTexto(listadoPersonas);
+       listadoPersonas = restablecerArreglo( listadoPersonas );
+       imprimirArregloTexto( listadoPersonas);
+
+
+    }
+
+    // función con array:
+    public static void imprimirArreglo(int [] arreglo ){ // indicar la dimensión antes del nombre del parámetro
+        for( int i = 0; i < arreglo.length; i++){
+            System.out.print(" [ "+arreglo[i]+" ] ");
+        }
+        System.out.println("");
+    }
+
+    public static int[] llenarArreglo(int [] arreglo){
+        for(int i = 0; i < arreglo.length; i++){
+            int aleatorio = (int) (Math.random()*100);
+            arreglo[i] = aleatorio; // En la posición del arreglo, recíbame los aleatorios
+        }
+        return arreglo;
+    }
+
+    // función con String:
+    public static String[] restablecerArreglo( String [] arreglo){
+        for (int i = 0; i < arreglo.length; i++){
+            arreglo[i] = "Llenito";
+        }
+        return arreglo;
+    }
+
+    public static void imprimirArregloTexto(String [] arreglo ){ // indicar la dimensión antes del nombre del parámetro
+        for( int i = 0; i < arreglo.length; i++){
+            System.out.print(" [ "+arreglo[i]+" ] ");
+        }
+        System.out.println("");
+    }
+
+
+
+}
